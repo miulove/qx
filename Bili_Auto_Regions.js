7183cf8a702e9dffca966d800bc4abc2f874c94b
栗栗小猫 <45760201+miulove@users.noreply.github.com>
2022-03-03 09:20:22 +0800

    Update Bili_Auto_Regions.js

--- Bili_Auto_Regions.js
+++ Bili_Auto_Regions.js
@@ -79,7 +79,7 @@ async function SwitchRegion(play) {
 	const Group = $.read('BiliArea_Policy') || '哔哩哔哩'; //Your blibli policy group name.
 	const CN = $.read('BiliArea_CN') || 'DIRECT'; //Your China sub-policy name.
 	const TW = $.read('BiliArea_TW') || '台湾04'; //Your Taiwan sub-policy name.
-	const HK = $.read('BiliArea_HK') || '香港03'; //Your HongKong sub-policy name.
+	const HK = $.read('BiliArea_HK') || '香港01'; //Your HongKong sub-policy name.
 	const DF = $.read('BiliArea_DF') || 'DIRECT'; //Sub-policy name used after region is blocked(e.g. url 404)
 	const off = $.read('BiliArea_disabled') || ''; //WiFi blacklist(disable region change), separated by commas.
 	const current = await $.getPolicy(Group);
